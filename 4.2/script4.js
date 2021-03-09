c0252270155e20e63e600b5040b429ac7d4cf375
vitalik495 <79410499+vitalik495@users.noreply.github.com>
2021-03-09 22:01:12 +0200

    Add files via upload

---
+++ 4.2/script4.js
@@ -0,0 +1,85 @@
+const students = ["Олександр", "Ігор", "Олена", "Іра", "Олексій", "Світлана"]
+const themes = ["Диференційне рівняння", "Теорія автоматів", "Алгоритми і структури даних"]
+const marks = [4, 5, 5, 3, 4, 5]
+
+//#1
+function getPairs(arr) {
+  const studentsArray = [...arr]
+  const girls = []
+  const boys = []
+  const pair = []
+
+  for (let i = 0; i < studentsArray.length; i++) {
+    if (studentsArray[i].endsWith("а")) {
+      girls.push(studentsArray[i])
+    } else {
+      boys.push(studentsArray[i])
+      }
+  }
+
+  for (let i = 0; i < girls.length; i++) {
+    pair.push( [boys[i], girls[i]] )
+  }
+
+  return pair
+}
+
+//#2
+function givingThemes(pairs, themes) {
+  const studentsThemes = []
+
+  for(let i = 0; i < pairs.length; i++){
+    studentsThemes.push ([pairs[i].join(" i "),themes[i]])
+  }
+
+  return studentsThemes
+}
+
+const pairs =  getPairs(students) 
+const allTheme = givingThemes(pairs, themes)
+
+//#3
+function givingTask(students, marks) {
+  const studentsTask = []
+
+  for(let i = 0; i<students.length; i++){
+    studentsTask.push(  [ students[i], marks[i] ])
+  }
+
+  return studentsTask
+}
+
+const lastmarks = givingTask(students , marks)
+
+//#4
+function getNamesThemeTask(allTheme, marks){
+  const namesWThemesWMarks = []
+
+  for(let i = 0; i < allTheme.length; i++){
+    namesWThemesWMarks.push( [ ...allTheme[i], marks[i] ] )
+     
+  }
+  return namesWThemesWMarks
+}
+
+const dataNamesWThemesWMarks = getNamesThemeTask(allTheme, marks)
+
+console.log(pairs)
+console.log(allTheme)
+console.log(lastmarks)
+console.log(dataNamesWThemesWMarks);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
